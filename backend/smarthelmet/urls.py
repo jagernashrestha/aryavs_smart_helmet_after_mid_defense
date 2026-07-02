0ed9d8f3bcb3176fb52f374bc74ecde5c1c75df5
jagernashrestha <jagerna1543@gmail.com>
2026-07-02 14:41:05 +0545

    updates

--- backend/smarthelmet/urls.py
+++ backend/smarthelmet/urls.py
@@ -2,8 +2,11 @@
 SmartHelmetX project URL configuration.
 """
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    # Redirect root to /api/
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     # All API endpoints defined in the api app
     path('api/', include('api.urls')),
 ]
